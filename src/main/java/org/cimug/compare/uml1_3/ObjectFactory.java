7daf94d3240830bf2fd06d3958ea9c951f686576
Todd Viegut <cimug.dev@gmail.com>
2020-04-10 08:21:05 -0500

    Updates to invalid package namespaces.

--- src/main/java/org/cimug/compare/uml1_3/ObjectFactory.java
+++ src/main/java/org/cimug/compare/uml1_3/ObjectFactory.java
@@ -14,7 +14,7 @@ import javax.xml.namespace.QName;
 
 /**
  * This object contains factory methods for each Java content interface and Java
- * element interface generated in the com.osii.cim.uml1_3 package.
+ * element interface generated in the org.cimug.compare.uml1_3 package.
  * <p>
  * An ObjectFactory allows you to programatically construct new instances of the
  * Java representation for XML content. The Java representation of XML content
@@ -57,7 +57,7 @@ public class ObjectFactory {
 
 	/**
 	 * Create a new ObjectFactory that can be used to create new instances of schema
-	 * derived classes for package: com.osii.cim.uml1_3
+	 * derived classes for package: org.cimug.compare.uml1_3
 	 * 
 	 */
 	public ObjectFactory() {
--- src/main/java/org/cimug/compare/xmi1_1/ObjectFactory.java
+++ src/main/java/org/cimug/compare/xmi1_1/ObjectFactory.java
@@ -11,7 +11,7 @@ import javax.xml.bind.annotation.XmlRegistry;
 
 /**
  * This object contains factory methods for each Java content interface and Java
- * element interface generated in the com.osii.cim.xmi1_1 package.
+ * element interface generated in the org.cimug.compare.xmi1_1 package.
  * <p>
  * An ObjectFactory allows you to programatically construct new instances of the
  * Java representation for XML content. The Java representation of XML content
@@ -25,7 +25,7 @@ public class ObjectFactory {
 
 	/**
 	 * Create a new ObjectFactory that can be used to create new instances of schema
-	 * derived classes for package: com.osii.cim.xmi1_1
+	 * derived classes for package: org.cimug.compare.xmi1_1
 	 * 
 	 */
 	public ObjectFactory() {
